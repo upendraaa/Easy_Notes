3ecccda06f5ea0f6470bb0c0ea1eb03724a3f53a
upendraaa <upendraaa@gmail.com>
2020-07-08 23:19:56 +0530

    Minor changes

--- git_projects/AndroidBackground/app/src/main/java/com/d4static/background/BackgroundTaskViewModel.kt
+++ git_projects/AndroidBackground/app/src/main/java/com/d4static/background/BackgroundTaskViewModel.kt
@@ -4,11 +4,13 @@ import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class BackgroundTaskViewModel : ViewModel() {
 
     fun doImmediateTask() {
 
+        //Global scope is like demon thread for co-routines
         GlobalScope.launch {
             delay(1000L)
             var mul = 100 * 100;
@@ -17,5 +19,15 @@ class BackgroundTaskViewModel : ViewModel() {
 
         println("Square of 100,")
 
+    }
+
+    //Launching 10K coroutines
+    fun repeatDot() = runBlocking {
+        repeat(10_000) {
+            launch {
+                delay(100L)
+                print(".")
+            }
+        }
     }
 }
